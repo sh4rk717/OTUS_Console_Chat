cab9a6cefa20e2683a98d700ca7650f8f0e4f644
Denis Kuydinov <QDen84@gmail.com>
2025-03-07 23:10:37 +0300

    3 new commands. list of tasks

--- Program.cs
+++ Program.cs
@@ -5,19 +5,20 @@ internal static class Program
 	private static void Main()
 	{
 		var userName = "";
+		List<String> tasks = new List<String>();
 		
 		while (true)
 		{
 			ShowMenu(userName);
 
 			Console.Write(userName != ""
-				? $"{userName}, введите одну из доступных команд: "
-				: "Введите одну из доступных команд: ");
+				? $"Привет, {userName}! Введите одну из доступных команд: "
+                : "Введите одну из доступных команд: ");
 			
 			Console.ForegroundColor = ConsoleColor.Green;
 			var userInput = Console.ReadLine() ?? throw new NullReferenceException();
 			Console.ResetColor();
-			var command = "";
+            var command = "";
 			if (userInput != "")
 			{
 				command = userInput.Split(" ")[0]; 
@@ -26,7 +27,7 @@ internal static class Program
 			switch (command)
 			{
 				case "/start":
-					RunStart(out userName);
+                    RunStart(out userName);
 					break;
 				case "/help":
 					RunHelp(userName);
@@ -36,6 +37,18 @@ internal static class Program
 					break;
 				case "/echo":
 					RunEcho(userName, userInput);
+					break;
+				case "/addtask":
+				case "/at":
+					AddTask(tasks);
+					break;
+				case "/showtasks":
+				case "/st":
+					ShowTasks(tasks);
+					break;
+				case "/removetask":
+				case "/rt":
+					RemoveTask(tasks);
 					break;
 				case "/exit":
 				case "q":
@@ -51,17 +64,22 @@ internal static class Program
 		Console.WriteLine("/start");
 		Console.WriteLine("/help");
 		Console.WriteLine("/info");
-		Console.WriteLine("/exit");
+		Console.WriteLine("/exit or q");
 		if (userName != "")
+		{
 			Console.WriteLine("/echo");
-		Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("/addtask or /at");
+			Console.WriteLine("/showtasks or /st");
+			Console.WriteLine("/removetask or /rt");
+		}
+		Console.ResetColor();
 	}
 
 	private static void RunStart(out string userName)
 	{
 		do
 		{
-			Console.Write("Введите ваше имя: ");
+			Console.Write("Пожалуйста, введите ваше имя: ");
 			userName = Console.ReadLine() ?? throw new NullReferenceException();
 		}
 		while (userName.Length < 2);
@@ -71,7 +89,12 @@ internal static class Program
 	{
 		if (userName != "")
 			Console.WriteLine($"\n\t\tCurrent user: {userName}");
-		Console.WriteLine("Программа имитирует Telegram-чат\nПользователю доступен набор команд.\n...some text...");
+		Console.WriteLine("Программа имитирует Telegram-чат\nПользователю доступен набор команд...\n...some text...");
+		Console.WriteLine("Команды доступные в чате после ввода имени пользователя:");
+		Console.WriteLine("\t/echo       - печатает в ответ переданный ей текст;");
+		Console.WriteLine("\t/addtask    - позволяет добавить задачу в список задач;");
+		Console.WriteLine("\t/showtasks  - позволяет просмотреть текущий список задач;");
+		Console.WriteLine("\t/removetask - позволяет удалить задачу из списка задач");
 		Console.WriteLine("Press any key to continue...");
 		Console.ReadKey();
 	}
@@ -102,4 +125,71 @@ internal static class Program
 		Console.ReadKey();
 	}
 	
+	private static void AddTask(List<string> tasks)
+	{
+		Console.Write("Введите описание задачи: ");
+		Console.ForegroundColor = ConsoleColor.Yellow;
+		var newTask = Console.ReadLine();
+		Console.ResetColor();
+		if (newTask != null) tasks.Add(newTask);
+		Console.WriteLine($"Задача \"{newTask}\" добавлена в список под номером {tasks.Count}");
+		Console.WriteLine("Press any key to continue...");
+		Console.ReadKey();
+	}
+	
+	private static void ShowTasks(List<string> tasks)
+	{
+		Console.Write("Ваш список задач: ");
+		Console.ForegroundColor = ConsoleColor.DarkYellow;
+		if (tasks.Count == 0) Console.WriteLine("пуст");
+		else
+		{
+			Console.WriteLine();
+			for (int i = 0; i < tasks.Count; i++)
+			{
+				Console.WriteLine($"{i + 1}. {tasks[i]}");
+			}
+		}
+		Console.WriteLine("Press any key to continue...");
+		Console.ReadKey();
+	}
+	
+	private static void RemoveTask(List<string> tasks)
+	{
+		Console.ForegroundColor = ConsoleColor.DarkYellow;
+		if (tasks.Count == 0)
+		{
+			Console.WriteLine("Ваш список задач пуст\nНет задач для удаления");
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+			return;
+		}
+		
+		Console.WriteLine("Вот ваш список задач: ");
+		for (int i = 0; i < tasks.Count; i++)
+		{
+			Console.WriteLine($"{i + 1}. {tasks[i]}");
+		}
+
+		Console.Write("Введите номер задачи для удаления: ");
+		int numberToRemove = 0;
+		var isInt = false;
+		while (numberToRemove <= 0 || numberToRemove > tasks.Count || !isInt)
+		{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				var userInput = Console.ReadLine();
+				if (userInput == "q") return;
+				isInt = int.TryParse(userInput, out numberToRemove);
+				if (numberToRemove <= 0 || numberToRemove > tasks.Count || !isInt)
+				{
+					Console.WriteLine("Некорректный номер задачи для удаления");
+					Console.Write("Введите номер задачи или нажмите q для выхода из режима удаления: ");
+				}
+		}
+		var taskToRemove = tasks[numberToRemove - 1];
+		tasks.RemoveAt(numberToRemove - 1);
+		Console.WriteLine($"Задача \"{taskToRemove}\" удалена.");
+		Console.WriteLine("Press any key to continue...");
+		Console.ReadKey();
+	}
 }
