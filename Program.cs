7d10afc4b38af21184e10761f6fc660f4e1c6d77
Denis Kuydinov <QDen84@gmail.com>
2025-03-11 01:00:33 +0300

    exceptions homework

--- Program.cs
+++ Program.cs
@@ -2,194 +2,270 @@
 
 internal static class Program
 {
-	private static void Main()
-	{
-		var userName = "";
-		List<String> tasks = new List<String>();
-		
-		while (true)
-		{
-			ShowMenu(userName);
+    private static void Main()
+    {
+        var userName = "";
+        List<string> tasks = [];
 
-			Console.Write(userName != ""
-				? $"Привет, {userName}! Введите одну из доступных команд: "
-                : "Введите одну из доступных команд: ");
-			
-			Console.ForegroundColor = ConsoleColor.Green;
-			var userInput = Console.ReadLine() ?? throw new NullReferenceException();
-			Console.ResetColor();
-            var command = "";
-			if (userInput != "")
-			{
-				command = userInput.Split(" ")[0]; 
-			}
+        Console.Write("Введите максимально допустимое количество задач: ");
+        int maxTasks = ParseAndValidateInt(Console.ReadLine(), 1, 100);
+        
+        Console.Write("Введите максимально допустимую длину задачи: ");
+        int maxTaskLength = ParseAndValidateInt(Console.ReadLine(), 1, 100);
 
-			switch (command)
-			{
-				case "/start":
-                    RunStart(out userName);
-					break;
-				case "/help":
-					RunHelp(userName);
-					break;
-				case "/info":
-					RunInfo(userName);
-					break;
-				case "/echo":
-					RunEcho(userName, userInput);
-					break;
-				case "/addtask":
-				case "/at":
-					AddTask(tasks);
-					break;
-				case "/showtasks":
-				case "/st":
-					ShowTasks(tasks);
-					break;
-				case "/removetask":
-				case "/rt":
-					RemoveTask(tasks);
-					break;
-				case "/exit":
-				case "q":
-					return;
-			}
-		} 
-	}
+        while (true)
+        {
+            try
+            {
+                // throw new Exception();
+                // throw new ArgumentException();
+                // throw new TaskCountLimitException(5);
 
-	private static void ShowMenu(string userName = "")
-	{
-		Console.Clear();
-		Console.ForegroundColor = ConsoleColor.Yellow;
-		Console.WriteLine("/start");
-		Console.WriteLine("/help");
-		Console.WriteLine("/info");
-		Console.WriteLine("/exit or q");
-		if (userName != "")
-		{
-			Console.WriteLine("/echo");
-			Console.WriteLine("/addtask or /at");
-			Console.WriteLine("/showtasks or /st");
-			Console.WriteLine("/removetask or /rt");
-		}
-		Console.ResetColor();
-	}
+                ShowMenu(userName);
 
-	private static void RunStart(out string userName)
-	{
-		do
-		{
-			Console.Write("Пожалуйста, введите ваше имя: ");
-			userName = Console.ReadLine() ?? throw new NullReferenceException();
-		}
-		while (userName.Length < 2);
-	}
+                Console.Write(userName != ""
+                    ? $"Привет, {userName}! Введите одну из доступных команд: "
+                    : "Введите одну из доступных команд: ");
 
-	private static void RunHelp(string userName)
-	{
-		if (userName != "")
-			Console.WriteLine($"\n\t\tCurrent user: {userName}");
-		Console.WriteLine("Программа имитирует Telegram-чат\nПользователю доступен набор команд...\n...some text...");
-		Console.WriteLine("Команды доступные в чате после ввода имени пользователя:");
-		Console.WriteLine("\t/echo       - печатает в ответ переданный ей текст;");
-		Console.WriteLine("\t/addtask    - позволяет добавить задачу в список задач;");
-		Console.WriteLine("\t/showtasks  - позволяет просмотреть текущий список задач;");
-		Console.WriteLine("\t/removetask - позволяет удалить задачу из списка задач");
-		Console.WriteLine("Press any key to continue...");
-		Console.ReadKey();
-	}
+                Console.ForegroundColor = ConsoleColor.Green;
+                var userInputCommand = ValidateString(Console.ReadLine());
+                Console.ResetColor();
+                var command = "";
+                if (userInputCommand != "")
+                {
+                    command = userInputCommand.Split(" ")[0];
+                }
 
-	private static void RunInfo(string userName)
-	{
-		if (userName != "")
-			Console.WriteLine($"\n\t\tCurrent user: {userName}");
-		Console.WriteLine("Program info: version 1.0b.\nCreated: Feb 18, 2025");
-		Console.WriteLine("Press any key to continue...");
-		Console.ReadKey();
-	}
+                switch (command)
+                {
+                    case "/start":
+                        RunStart(out userName);
+                        break;
+                    case "/help":
+                        RunHelp(userName);
+                        break;
+                    case "/info":
+                        RunInfo(userName);
+                        break;
+                    case "/echo":
+                        RunEcho(userName, userInputCommand);
+                        break;
+                    case "/addtask":
+                    case "/at":
+                        AddTask(tasks, maxTasks, maxTaskLength);
+                        break;
+                    case "/showtasks":
+                    case "/st":
+                        ShowTasks(tasks);
+                        break;
+                    case "/removetask":
+                    case "/rt":
+                        RemoveTask(tasks);
+                        break;
+                    case "/exit":
+                    case "q":
+                        return;
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            catch (TaskCountLimitException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            catch (TaskLengthLimitException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            catch (DuplicateTaskException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Произошла непредвиденная ошибка: ");
+                Console.WriteLine(
+                    $"e.GetType = {e.GetType()}\ne.Message = {e.Message}\ne.StackTrace = {e.StackTrace}\ne.InnerException = {e.InnerException}");
+                Console.ResetColor();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+    }
 
-	private static void RunEcho(string userName, string? userInput)
-	{
-		if (userName != "")
-			Console.WriteLine($"\n\t\tCurrent user: {userName}");
-		
-		if (userInput == "/echo")
-			Console.WriteLine("Программа ожидает текст после команды /echo");
+    private static void ShowMenu(string userName = "")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("/start");
+        Console.WriteLine("/help");
+        Console.WriteLine("/info");
+        Console.WriteLine("/exit or q");
+        if (userName != "")
+        {
+            Console.WriteLine("/echo");
+            Console.WriteLine("/addtask or /at");
+            Console.WriteLine("/showtasks or /st");
+            Console.WriteLine("/removetask or /rt");
+        }
 
-		if (userInput is { Length: > 6 })
-		{
-			Console.WriteLine($"You entered: {userInput[6..]}");
-		}
+        Console.ResetColor();
+    }
 
-		Console.WriteLine("Press any key to continue...");
-		Console.ReadKey();
-	}
-	
-	private static void AddTask(List<string> tasks)
-	{
-		Console.Write("Введите описание задачи: ");
-		Console.ForegroundColor = ConsoleColor.Yellow;
-		var newTask = Console.ReadLine();
-		Console.ResetColor();
-		if (newTask != null) tasks.Add(newTask);
-		Console.WriteLine($"Задача \"{newTask}\" добавлена в список под номером {tasks.Count}");
-		Console.WriteLine("Press any key to continue...");
-		Console.ReadKey();
-	}
-	
-	private static void ShowTasks(List<string> tasks)
-	{
-		Console.Write("Ваш список задач: ");
-		Console.ForegroundColor = ConsoleColor.DarkYellow;
-		if (tasks.Count == 0) Console.WriteLine("пуст");
-		else
-		{
-			Console.WriteLine();
-			for (int i = 0; i < tasks.Count; i++)
-			{
-				Console.WriteLine($"{i + 1}. {tasks[i]}");
-			}
-		}
-		Console.WriteLine("Press any key to continue...");
-		Console.ReadKey();
-	}
-	
-	private static void RemoveTask(List<string> tasks)
-	{
-		Console.ForegroundColor = ConsoleColor.DarkYellow;
-		if (tasks.Count == 0)
-		{
-			Console.WriteLine("Ваш список задач пуст\nНет задач для удаления");
-			Console.WriteLine("Press any key to continue...");
-			Console.ReadKey();
-			return;
-		}
-		
-		Console.WriteLine("Вот ваш список задач: ");
-		for (int i = 0; i < tasks.Count; i++)
-		{
-			Console.WriteLine($"{i + 1}. {tasks[i]}");
-		}
+    private static void RunStart(out string userName)
+    {
+        do
+        {
+            Console.Write("Пожалуйста, введите ваше имя: ");
+            userName = ValidateString(Console.ReadLine());
+        } while (userName.Length < 2);
+    }
 
-		Console.Write("Введите номер задачи для удаления: ");
-		int numberToRemove = 0;
-		var isInt = false;
-		while (numberToRemove <= 0 || numberToRemove > tasks.Count || !isInt)
-		{
-				Console.ForegroundColor = ConsoleColor.Yellow;
-				var userInput = Console.ReadLine();
-				if (userInput == "q") return;
-				isInt = int.TryParse(userInput, out numberToRemove);
-				if (numberToRemove <= 0 || numberToRemove > tasks.Count || !isInt)
-				{
-					Console.WriteLine("Некорректный номер задачи для удаления");
-					Console.Write("Введите номер задачи или нажмите q для выхода из режима удаления: ");
-				}
-		}
-		var taskToRemove = tasks[numberToRemove - 1];
-		tasks.RemoveAt(numberToRemove - 1);
-		Console.WriteLine($"Задача \"{taskToRemove}\" удалена.");
-		Console.WriteLine("Press any key to continue...");
-		Console.ReadKey();
-	}
+    private static void RunHelp(string userName)
+    {
+        if (userName != "")
+            Console.WriteLine($"\n\t\tCurrent user: {userName}");
+        Console.WriteLine("Программа имитирует Telegram-чат\nПользователю доступен набор команд...\n...some text...");
+        Console.WriteLine("Команды доступные в чате после ввода имени пользователя:");
+        Console.WriteLine("\t/echo       - печатает в ответ переданный ей текст;");
+        Console.WriteLine("\t/addtask    - позволяет добавить задачу в список задач;");
+        Console.WriteLine("\t/showtasks  - позволяет просмотреть текущий список задач;");
+        Console.WriteLine("\t/removetask - позволяет удалить задачу из списка задач");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private static void RunInfo(string userName)
+    {
+        if (userName != "")
+            Console.WriteLine($"\n\t\tCurrent user: {userName}");
+        Console.WriteLine("Program info: version 1.0b.\nCreated: Feb 18, 2025");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private static void RunEcho(string userName, string? userInput)
+    {
+        if (userName != "")
+            Console.WriteLine($"\n\t\tCurrent user: {userName}");
+
+        if (userInput == "/echo")
+            Console.WriteLine("Программа ожидает текст после команды /echo");
+
+        if (userInput is { Length: > 6 })
+        {
+            Console.WriteLine($"You entered: {userInput[6..]}");
+        }
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private static void AddTask(List<string> tasks, int maxTasks, int maxTaskLength)
+    {
+        if (tasks.Count >= maxTasks)
+            throw new TaskCountLimitException(maxTasks);
+        Console.Write("Введите описание задачи: ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        var newTask = ValidateString(Console.ReadLine());
+        Console.ResetColor();
+        if (newTask.Length > maxTaskLength)
+            throw new TaskLengthLimitException(newTask.Length, maxTaskLength);
+        if (tasks.Contains(newTask))
+            throw new DuplicateTaskException(newTask);
+        tasks.Add(newTask);
+        Console.WriteLine($"Задача \"{newTask}\" добавлена в список под номером {tasks.Count}");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private static void ShowTasks(List<string> tasks)
+    {
+        Console.Write("Ваш список задач: ");
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        if (tasks.Count == 0) Console.WriteLine("пуст");
+        else
+        {
+            Console.WriteLine();
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tasks[i]}");
+            }
+        }
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private static void RemoveTask(List<string> tasks)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        if (tasks.Count == 0)
+        {
+            Console.WriteLine("Ваш список задач пуст\nНет задач для удаления");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("Вот ваш список задач: ");
+        for (var i = 0; i < tasks.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {tasks[i]}");
+        }
+
+        Console.Write("Введите номер задачи для удаления: ");
+        var numberToRemove = 0;
+        var isInt = false;
+        while (numberToRemove <= 0 || numberToRemove > tasks.Count || !isInt)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var userInputTaskToRemove = ValidateString(Console.ReadLine());
+            if (userInputTaskToRemove == "q") return;
+            isInt = int.TryParse(userInputTaskToRemove, out numberToRemove);
+            if (numberToRemove > 0 && numberToRemove <= tasks.Count && isInt) continue;
+            Console.WriteLine("Некорректный номер задачи для удаления");
+            Console.Write("Введите номер задачи или нажмите q для выхода из режима удаления: ");
+        }
+
+        var taskToRemove = tasks[numberToRemove - 1];
+        tasks.RemoveAt(numberToRemove - 1);
+        Console.WriteLine($"Задача \"{taskToRemove}\" удалена.");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private static int ParseAndValidateInt(string? str, int min, int max)
+    {
+        var isNumber = int.TryParse(str, out var number);
+        if (!isNumber || number < min || number > max)
+            throw new ArgumentException();
+        return number;
+    }
+
+    private static string ValidateString(string? str)
+    {
+        if (str != null && str.Replace(" ", "").Replace("\t", "").Length > 0)
+            return str;
+        throw new ArgumentException();
+    }
 }
---
+++ UserExceptions.cs
@@ -0,0 +1,7 @@
+﻿namespace FirstInteract;
+
+public class TaskCountLimitException(int taskCountLimit) : Exception($"Превышено максимальное количество задач равное {taskCountLimit}");
+
+public class TaskLengthLimitException(int taskLength, int taskLengthLimit) : Exception($"Длина задачи '{taskLength}' превышает максимально допустимое значение {taskLengthLimit}");
+
+public class DuplicateTaskException(string task) : Exception($"Задача '{task}' уже существует");
